45bd4649f2b1d7b86023030a4641ec22a3ce71f9
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2017-10-25 09:22:41 +0300

    pagination added

--- pageNavigator/ViewController.swift
+++ pageNavigator/ViewController.swift
@@ -8,11 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    // FirstSliderViewController
+    // SecondSliderViewController
+    // ThirdSliderViewController
+    
+    lazy var viewControllerList: [UIViewController] = {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let viewController1 = storyBoard.instantiateViewController(withIdentifier: "FirstSliderViewController")
+        
+        let viewController2 = storyBoard.instantiateViewController(withIdentifier: "SecondSliderViewController")
+        
+        let viewController3 = storyBoard.instantiateViewController(withIdentifier: "ThirdSliderViewController")
+        
+        
+        return [viewController1, viewController2, viewController3]
+    }()
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard viewControllerList.count > previousIndex else {
+            return nil
+        }
+        
+        return viewControllerList[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {
+            return nil
+        }
+
+        guard viewControllerList.count > nextIndex else {
+            return nil
+        }
+        
+        return viewControllerList[nextIndex]
+    }
+    
+//    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+//        return viewControllerList.count
+//    }
+//    
+//    
+//    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        guard let firstViewController = viewControllers?.first,
+//            let firstViewControllerIndex = viewControllerList.index(of: firstViewController) else {
+//            return 0
+//        }
+//        
+//        return firstViewControllerIndex
+//    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        delegate = self
+        dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
